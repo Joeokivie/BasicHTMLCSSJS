312e956aeb4e754b4264549e037069b01e4fb4d6
joeokivie <josephokivie@hotmail.com>
2024-01-26 20:48:13 -0500

    style the responsive nav

---
+++ script.js
@@ -0,0 +1,20 @@
+console.log(`Hello from Grailed`);
+
+// let users = [
+//     {
+//         id: 1,
+//         name: `joseph`,
+//         status: `online`
+//     },
+//     {
+//         id: 2,
+//         name: `rakib`,
+//         status: `offline`
+//     }
+// ]
+
+// console.log(`Users`, users);
+
+// users = users.filter(usr => usr.name == `joseph`);
+
+// console.log(`Updated Users`, users);
